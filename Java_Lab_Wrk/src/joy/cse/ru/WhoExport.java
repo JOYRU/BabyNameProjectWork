44fe67c89f89cc566ea7bb4f0c6a93a3db61064f
JOY_RU <joycseru@gmail.com>
2017-11-04 09:27:39 +0600

    countryInfo method is complete

--- Java_Lab_Wrk/src/joy/cse/ru/WhoExport.java
+++ Java_Lab_Wrk/src/joy/cse/ru/WhoExport.java
@@ -1,10 +1,40 @@
 package joy.cse.ru;
+import org.apache.commons.csv.*;
+import edu.duke.*;
 
 public class WhoExport {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WhoExport f = new WhoExport() ;
+		f.testAllMethod() ;
 
+	}
+	public String  countryInfo(CSVParser parser , String country ){
+		
+		for(CSVRecord record : parser){
+			
+			if(record.get("Country").equals(country))
+			{
+				String ob ;
+				ob = record.get("Country") + ": " + record.get("Exports")+": " + record.get("Value (dollars)") ;
+				return ob ;
+			}
+		}
+		
+		return "NOT FOUND";
+	} 
+	
+	public void testAllMethod(){
+		
+		FileResource fr = new FileResource() ;
+		CSVParser parser = fr.getCSVParser();
+		
+		System.out.println(countryInfo(parser , "Germany"));
+		
+		parser = fr.getCSVParser();
+		
+		//listExportersTwoProducts( parser , String exportItem1 , String exportItem2 ) ;
+		
 	}
 
 }
